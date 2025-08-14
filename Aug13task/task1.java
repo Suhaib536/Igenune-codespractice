96c36f5e5d33bc26525b7b947a1c18fdbf5c804a
Suhaib536 <suhaibshafijan@gmail.com>
2025-08-14 09:42:56 +0530

    updated again

--- Aug13task/task1.java
+++ Aug13task/task1.java
@@ -1,33 +1,36 @@
 package Aug13task;
-
-import java.util.Stack;
-
+import java.util.*;
 public class task1 {
     public static void main(String[] args) {
-        Stack<String> s =new Stack<>();
-        Stack<String> output = new Stack<>();
-        Stack<String> out = new Stack<>();
-        Stack<Integer> num = new Stack<>();
-
-        s.push("3[a]2[bc]");
-        String stack= s.peek();
-        for(int i=0; i <stack.length();i++){
-            output.push(String.valueOf(stack.charAt(i)));
-        }//input = 3[a]2[bc]
-        //output = aaabcbc-code leetcode -394
-        System.out.println(output);
-
-        String fp = output.peek();
-        while(!output.isEmpty()){
-            if(output.peek().equals(fp)){
-                output.pop();
+        String s = "3[a]2[bc]";
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> strStack = new Stack<>();
+        String curr = "";
+        int k = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                k = k * 10 + (ch - '0');
+            } 
+            else if (ch == '[') {
+                countStack.push(k);
+                strStack.push(curr);
+                curr = "";
+                k = 0;
+            } 
+            else if (ch == ']') {
+                int repeatTimes2 = countStack.pop();
+                String prev2 = strStack.pop();
+                StringBuilder temp = new StringBuilder();
+            for (int j = 0; j < repeatTimes2; j++) {
+                temp.append(curr);
             }
-            else{
-                out.push(output.pop());
+            curr = prev2 + temp.toString();
+            } 
+            else {
+                curr += ch;
             }
         }
-        System.out.println(output);
-        s.push(out.pop());
-        String d  = out.pop();
+        System.out.println(curr);
     }
 }
